cffa0348a0b86cf775b491a86763b1704517ccfb
Theo  Rambaud <theo.rambaud@capgemini.com>
2020-05-10 11:04:01 +0200

    feat(ms-edge) : ms gateway (+ keycloak)

---
+++ application/ms-edge/ms-gateway/src/main/java/com/wildadventures/msgateway/PreFlightCorsConfiguration.java
@@ -0,0 +1,30 @@
+package com.wildadventures.msgateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class PreFlightCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter( ) {
+        return new CorsWebFilter( corsConfigurationSource( ) );
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource( ) {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource( );
+        CorsConfiguration config = new CorsConfiguration( ).applyPermitDefaultValues( );
+        config.addAllowedMethod( HttpMethod.GET );
+        config.addAllowedMethod( HttpMethod.PUT );
+        config.addAllowedMethod( HttpMethod.POST );
+        config.addAllowedMethod( HttpMethod.DELETE );
+        source.registerCorsConfiguration( "/**", config );
+        return source;
+    }
+}
