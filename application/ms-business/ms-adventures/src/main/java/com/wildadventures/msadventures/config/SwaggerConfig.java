f0a57f8d0405ec41eae17f8611c0f5d71671a684
Nathanaël Morel <nathanael.morel@outlook.fr>
2020-05-10 12:50:26 +0200

    fix swagger

--- application/ms-business/ms-adventures/src/main/java/com/wildadventures/msadventures/config/SwaggerConfig.java
+++ application/ms-business/ms-adventures/src/main/java/com/wildadventures/msadventures/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.aventuressauvages.msaventures"))
+                .apis(RequestHandlerSelectors.basePackage("com.wildadventures.msadventures"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- application/ms-business/ms-comments/src/main/java/com/wildadventures/mscomments/config/SwaggerConfig.java
+++ application/ms-business/ms-comments/src/main/java/com/wildadventures/mscomments/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.wildadventures.comments"))
+                .apis(RequestHandlerSelectors.basePackage("com.wildadventures.mscomments"))
                 .paths(PathSelectors.any())
                 .build();
     }
